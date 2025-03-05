575431e0a7a217b132c6dd2a678ed09e31cb9961
Igor Savostin <isavostin@elvees.com>
2025-03-06 00:35:02 +0300

    add new tasks for dublicate from Pasha L

--- LeetCode.Tests/1_task/FindDuplicateTest.cs
+++ LeetCode.Tests/1_task/FindDuplicateTest.cs
@@ -14,4 +14,22 @@ public class FindDuplicateTestArray
     {
         return FindDuplicate.FindDuplicates(array).ToArray();
     }
+
+
+    [TestCase(new[] { 4, 3, 2, 7, 8, 2, 3, 1 }, ExpectedResult = new[] { 4, 3, 2, 7, 8, 1 })]
+    [TestCase(new[] { 1, 1, 2 }, ExpectedResult = new[] { 1, 2 })]
+    [TestCase(new[] { 1 }, ExpectedResult = new int[] { 1 })]
+    public int[] RemoveDuplicatesTest(int[] array)
+    {
+        return FindDuplicate.RemoveDuplicates(array);
+    }
+
+
+    [TestCase(new[] { 4, 3, 2, 7, 8, 2, 3, 1 }, ExpectedResult = false)]
+    [TestCase(new[] { 1, 1, 2 }, ExpectedResult = true)]
+    [TestCase(new[] { 1 }, ExpectedResult = true)]
+    public bool IsUpperArrayTest(int[] array)
+    {
+        return FindDuplicate.IsUpperArray(array);
+    }
 }
--- LeetCode/1_task/FindDuplicate.cs
+++ LeetCode/1_task/FindDuplicate.cs
@@ -1,4 +1,6 @@
-﻿namespace LeetCode;
+﻿using System.Diagnostics;
+
+namespace LeetCode;
 
 /// <summary>
 /// https://leetcode.com/problems/find-all-duplicates-in-an-array/
@@ -36,4 +38,58 @@ public static class FindDuplicate
         }
         return list;
     }
+
+    /// <summary>
+    /// Формирование массива с уникальными значениями из входного массива
+    /// </summary>
+    //O(n^2)
+    public static int[] RemoveDuplicates(int[] nums)
+    {
+        int n = nums.Length;
+        int[] result = new int[n];
+        int count = 0;
+        result[0] = nums[0];
+
+        for (int i = 0; i < n; i++)
+        {
+            int j;
+            for (j = 0; j < count; j++)
+            {
+                if (nums[i] == result[j])
+                {
+                    break;
+                }
+            }
+            if (j == count)
+            {
+                result[count] = nums[i];
+                count++;
+            }
+
+        }
+        Array.Resize(ref result, count);
+        return result;
+    }
+
+    /// <summary>
+    /// Определить возростающая последовательность или нет
+    /// </summary>
+    //O(n)
+    public static bool IsUpperArray(int[] nums)
+    {
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.Length - 1)
+        {
+            right += 1;
+            if (nums[left] > nums[right])
+            {
+                return false;
+            }
+            left += 1;
+        }
+        return true;
+    }
+
 }
